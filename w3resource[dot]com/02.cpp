e335a7889025dde56fd8122b94eb022488503385
avinashankur <avinashhankur@gmail.com>
2023-11-17 16:48:11 +0530

    added:2

---
+++ w3resource[dot]com/02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// 2. Write a program in C++ to find the sum of the first 10 natural numbers.
+
+int main() {
+   int sum, n = 10;
+   sum = n * (n + 1) / 2;
+   
+   cout << sum;
+
+    return 0;
+}
